424ada67c206968f2f777fdab14cbb397e08c000
Sumit Nagariya <sumit.nagariya@flipkart.com>
2017-06-23 12:23:49 +0530

    bean change

--- src/main/java/com/ekart/e2e/test/service/apis/config/spring/ApplicationConfig.java
+++ src/main/java/com/ekart/e2e/test/service/apis/config/spring/ApplicationConfig.java
@@ -1,5 +1,7 @@
 package com.ekart.e2e.test.service.apis.config.spring;
 
+import com.ekart.e2e.test.service.service.BillServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -13,6 +15,11 @@ public class ApplicationConfig {
    // Reference:
    // http://docs.spring.io/spring-javaconfig/docs/1.0.0.M4/reference/html/ch04s02.html
    //Add your configs here
+
+    @Bean
+    public BillServiceImpl createBillService(){
+        return new BillServiceImpl();
+    }
 
 
 }
